6028a5a1b02915273fbc62473bab543f9421ab2c
Sam McMurray <scmcmurr@gmail.com>
2023-10-03 22:08:05 -0400

    Cool box shadow effects

--- src/components/squares/occupied/Preview.tsx
+++ src/components/squares/occupied/Preview.tsx
@@ -1,4 +1,4 @@
-import { Dialog } from "@mui/material";
+import { Box, Dialog, DialogContent, DialogTitle } from "@mui/material";
 import { Square } from "../../../hooks/useGetTestWorldCanvas";
 
 interface PreviewProps {
@@ -9,8 +9,26 @@ interface PreviewProps {
 
 const Preview = ({square, open, handleClose}: PreviewProps) => {
   return (
-    <Dialog open={open} onClose={handleClose}>
-      YAAAA
+    <Dialog open={open} onClose={handleClose} maxWidth="lg" fullWidth
+      PaperProps={{
+        style:{
+        boxShadow: `2px 2px 200px 2px ${square.color}`    
+      }}}
+    >
+      <DialogTitle>Post</DialogTitle>
+      <DialogContent>
+        <Box display="flex" >
+          <Box flexGrow={1} overflow="hidden">    
+              <p>
+              post content will go here aye hjubnuncineicvn cenchnbeubvh cerncjrnv aye hjubnuncineicvn cenchnbeubvh cerncjrnv aye hjubnuncineicvn cenchnbeubvh cerncjrnv aye hjubnuncineicvn cenchnbeubvh cerncjrnv cenchnbeubvh cerncjrnv aye hjubnuncineicvn cenchnbeubvh cerncjrnv end
+              </p>
+            
+          </Box>
+          <Box>
+            comment section
+          </Box>
+        </Box>
+      </DialogContent>
     </Dialog>
   )
 }
